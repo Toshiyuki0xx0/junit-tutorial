bb4d2f902563917fb709214616bb5b60f84038fe
ToshiyukiTerashima <t_toshiyuki@td5.so-net.ne.jp>
2019-11-10 17:54:55 +0900

    パラメーター化テスト

---
+++ test/junit/tutorial/ParameterizedTest.java
@@ -0,0 +1,23 @@
+package junit.tutorial;
+
+import org.junit.experimental.theories.DataPoint;
+import org.junit.experimental.theories.Theories;
+import org.junit.experimental.theories.Theory;
+import org.junit.runner.RunWith;
+
+@RunWith(Theories.class)
+public class ParameterizedTest {
+	@DataPoint
+	public static int INT_PARAM_1 = 3;
+	@DataPoint
+	public static int INT_PARAM_2 = 4;
+	
+	public ParameterizedTest() {
+		System.out.println("初期化");
+	}
+	@Theory
+	public void testCase(int param) throws Exception{
+		System.out.println("test(" + param +")");
+	}
+
+}
